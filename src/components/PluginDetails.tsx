caefbed05f43eb929197fd78039a74899eedb183
Elijah Green <elijah@infogata.com>
2022-10-16 13:45:28 -0700

    Add link to plugin manifest in plugin details

--- src/components/PluginDetails.tsx
+++ src/components/PluginDetails.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   List,
   ListItem,
+  ListItemButton,
   ListItemText,
   Typography,
 } from "@mui/material";
@@ -89,12 +90,20 @@ const PluginDetails: React.FC = () => {
                 secondary={`${playerSize / 1000} kb`}
               />
             </ListItem>
-            <ListItem>
-              <ListItemText
-                primary="Update Url"
-                secondary={plugin.manifestUrl}
-              />
-            </ListItem>
+            {plugin.manifestUrl && (
+              <ListItem>
+                <ListItemButton
+                  component="a"
+                  href={plugin.manifestUrl}
+                  target="_blank"
+                >
+                  <ListItemText
+                    primary="Update Url"
+                    secondary={plugin.manifestUrl}
+                  />
+                </ListItemButton>
+              </ListItem>
+            )}
           </List>
           {plugin.manifestUrl && <Button onClick={onUpdate}>Update</Button>}
         </div>
