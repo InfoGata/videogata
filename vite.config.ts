e4c35ef4c14ec9656e6cc1270f4869e9ccd27970
Elijah Green <elijah@infogata.com>
2023-03-31 16:28:52 -0700

    Setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
@@ -5,6 +6,10 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: ["fake-indexeddb/auto"],
+  },
   plugins: [
     react(),
     svgrPlugin(),
