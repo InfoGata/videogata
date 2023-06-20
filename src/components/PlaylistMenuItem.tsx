0abe0be823bbcc66c37424dc7dbf5f5f8ffb7c1a
Elijah Green <elijah@infogata.com>
2023-06-19 19:07:12 -0700

    Add confirmation message when adding videos to a playlist

--- src/components/PlaylistMenuItem.tsx
+++ src/components/PlaylistMenuItem.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { PlaylistInfo, Video } from "../plugintypes";
 import { useAppDispatch } from "../store/hooks";
 import { addPlaylistVideos } from "../store/reducers/playlistReducer";
+import { useTranslation } from "react-i18next";
+import { useSnackbar } from "notistack";
 
 interface PlaylistMenuItemProps {
   playlist: PlaylistInfo;
@@ -15,10 +17,18 @@ interface PlaylistMenuItemProps {
 const PlaylistMenuItem: React.FC<PlaylistMenuItemProps> = (props) => {
   const { playlist, closeMenu, videos, title } = props;
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+  const { enqueueSnackbar } = useSnackbar();
 
   const addToPlaylist = () => {
     if (playlist.id) {
       dispatch(addPlaylistVideos(playlist, videos));
+      enqueueSnackbar(
+        t("addedVideosToPlaylist", {
+          playlistName: playlist.name,
+          count: videos.length,
+        })
+      );
     }
     closeMenu();
   };
