99eb3ac4f7443989ed037c1499342b9f16d4e259
Elijah Green <elijah@infogata.com>
2023-10-28 22:29:01 -0700

    Fix styling of links

--- src/components/VideoDescrption.tsx
+++ src/components/VideoDescrption.tsx
@@ -1,30 +1,33 @@
-import { Typography } from "@mui/material";
-import DOMPurify from "dompurify";
+import { Link, Typography } from "@mui/material";
+import Linkify from "linkify-react";
 import React from "react";
 
 interface VideoDescrptionProps {
   description: string;
 }
 
+const renderLink = ({
+  attributes,
+  content,
+}: {
+  attributes: any;
+  content: any;
+}) => {
+  const { href, ...props } = attributes;
+  return (
+    <Link href={href} target="_blank" {...props}>
+      {content}
+    </Link>
+  );
+};
+
 const VideoDescrption: React.FC<VideoDescrptionProps> = (props) => {
   const { description } = props;
-  const urlRegex =
-    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
 
-  const descriptionWithLinks = description.replace(
-    urlRegex,
-    (url) => `<a href="${url}" target="_blank">${url}</a>`
-  );
-  const sanitizer = DOMPurify.sanitize;
   return (
-    <Typography
-      sx={{ whiteSpace: "pre-line" }}
-      component="div"
-      variant="body1"
-      dangerouslySetInnerHTML={{
-        __html: sanitizer(descriptionWithLinks, { ADD_ATTR: ["target"] }),
-      }}
-    />
+    <Typography sx={{ whiteSpace: "pre-line" }} component="div" variant="body1">
+      <Linkify options={{ render: renderLink }}>{description}</Linkify>
+    </Typography>
   );
 };
 export default React.memo(VideoDescrption);
