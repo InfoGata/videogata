0c341bccc7db0cbaa460387862e6124f3eab1b85
Elijah Green <elijah@infogata.com>
2024-02-19 21:25:01 -0800

    Fix tsc error

--- src/components/PlaylistSubMenu.tsx
+++ src/components/PlaylistSubMenu.tsx
@@ -1,8 +1,6 @@
 import { PlaylistInfo, Video } from "@/plugintypes";
-import { ListPlusIcon } from "lucide-react";
 import React from "react";
 import DropdownPlaylistMenuItem from "./DropdownPlaylistMenuItem";
-import { useTranslation } from "react-i18next";
 import {
   DropdownMenuPortal,
   DropdownMenuSub,
@@ -19,26 +17,20 @@ type Props = {
 const PlaylistSubMenu: React.FC<Props> = (props) => {
   const { playlists, videos, title } = props;
 
-  return (
-    playlists.length > 0 && (
-      <DropdownMenuSub>
-        <DropdownMenuSubTrigger>
-          <span>{title}</span>
-        </DropdownMenuSubTrigger>
-        <DropdownMenuPortal>
-          <DropdownMenuSubContent>
-            {playlists.map((p) => (
-              <DropdownPlaylistMenuItem
-                key={p.id}
-                playlist={p}
-                videos={videos}
-              />
-            ))}
-          </DropdownMenuSubContent>
-        </DropdownMenuPortal>
-      </DropdownMenuSub>
-    )
-  );
+  return playlists.length > 0 ? (
+    <DropdownMenuSub>
+      <DropdownMenuSubTrigger>
+        <span>{title}</span>
+      </DropdownMenuSubTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuSubContent>
+          {playlists.map((p) => (
+            <DropdownPlaylistMenuItem key={p.id} playlist={p} videos={videos} />
+          ))}
+        </DropdownMenuSubContent>
+      </DropdownMenuPortal>
+    </DropdownMenuSub>
+  ) : null;
 };
 
 export default PlaylistSubMenu;
