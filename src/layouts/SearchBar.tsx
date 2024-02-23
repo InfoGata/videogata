b768ae586d65e1c9ac64f82c12b525643b7c39dc
Elijah Green <elijah@infogata.com>
2024-02-23 01:13:29 -0800

    Add back autocomplete search bar

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -1,145 +1,166 @@
-import { Input } from "@/components/ui/input";
 import React from "react";
-import { useTranslation } from "react-i18next";
-import { CiSearch } from "react-icons/ci";
 import { useNavigate } from "react-router-dom";
+import {
+  CommandInput,
+  CommandList,
+  CommandGroup,
+  CommandItem,
+} from "../components/ui/command";
+import { Command as CommandPrimitive } from "cmdk";
+import { useAppSelector } from "@/store/hooks";
+import usePlugins from "@/hooks/usePlugins";
+import { PluginFrameContainer } from "@/PluginsContext";
+import { filterAsync } from "@/utils";
+import { debounce } from "lodash";
 
 const SearchBar: React.FC = () => {
-  // const currentPluginId = useAppSelector(
-  //   (state) => state.settings.currentPluginId
-  // );
-  //const { plugins } = usePlugins();
+  const currentPluginId = useAppSelector(
+    (state) => state.settings.currentPluginId
+  );
+  const { plugins } = usePlugins();
   const [search, setSearch] = React.useState("");
-  // const [currentSearch, setCurrentSearch] = React.useState("");
-  // const [options, setOptions] = React.useState<string[]>([]);
   const navigate = useNavigate();
-  const { t } = useTranslation();
-  // const [searchPlugin, setSearchPlugin] =
-  //   React.useState<PluginFrameContainer>();
+  const [open, setOpen] = React.useState(false);
+  const [searchPlugin, setSearchPlugin] =
+    React.useState<PluginFrameContainer>();
+  const [options, setOptions] = React.useState<string[]>([]);
+  const [selected, setSelected] = React.useState("");
 
-  const searchQuery = (searchTerm: string) => {
-    navigate(`/search?q=${searchTerm}`);
+  React.useEffect(() => {
+    const getSearchPlugin = async () => {
+      const validPlugins = await filterAsync(plugins, (p) =>
+        p.hasDefined.onGetSearchSuggestions()
+      );
+      const plugin = validPlugins.some((p) => p.id === currentPluginId)
+        ? validPlugins.find((p) => p.id === currentPluginId)
+        : validPlugins[0];
+      setSearchPlugin(plugin);
+    };
+    getSearchPlugin();
+  }, [currentPluginId, plugins]);
+
+  const onGetSuggestions = React.useCallback(
+    async (query: string) => {
+      if (searchPlugin) {
+        const suggestions = await searchPlugin.remote.onGetSearchSuggestions({
+          query,
+        });
+        return suggestions;
+      }
+    },
+    [searchPlugin]
+  );
+
+  const getSuggestionDebounce = React.useMemo(
+    () =>
+      debounce(
+        (
+          request: string,
+          callback: (suggestions: string[] | undefined) => void
+        ) => {
+          onGetSuggestions(request).then((data) => {
+            callback(data);
+          });
+        },
+        500
+      ),
+    [onGetSuggestions]
+  );
+
+  React.useEffect(() => {
+    let active = true;
+    if (search === "") {
+      setOptions([]);
+      return;
+    }
+    if (selected === search) {
+      return;
+    }
+
+    getSuggestionDebounce(search, (suggestions) => {
+      if (active) {
+        if (suggestions) {
+          setOptions(suggestions);
+        }
+      }
+    });
+
+    return () => {
+      active = false;
+    };
+  }, [search, selected, getSuggestionDebounce]);
+
+  const onValueChange = (value: string) => {
+    setSearch(value);
   };
 
-  const handleSubmit = (event: React.FormEvent<unknown>) => {
-    event.preventDefault();
-    searchQuery(search);
+  const handleBlur = () => {
+    setOpen(false);
   };
 
-  // React.useEffect(() => {
-  //   const getSearchPlugin = async () => {
-  //     const validPlugins = await filterAsync(plugins, (p) =>
-  //       p.hasDefined.onGetSearchSuggestions()
-  //     );
-  //     const plugin = validPlugins.some((p) => p.id === currentPluginId)
-  //       ? validPlugins.find((p) => p.id === currentPluginId)
-  //       : validPlugins[0];
-  //     setSearchPlugin(plugin);
-  //   };
-  //   getSearchPlugin();
-  // }, [currentPluginId, plugins]);
+  const searchQuery = (query: string) => {
+    navigate(`/search?q=${query}`);
+  };
 
-  // const onGetSuggestions = React.useCallback(
-  //   async (query: string) => {
-  //     if (searchPlugin) {
-  //       const suggestions = await searchPlugin.remote.onGetSearchSuggestions({
-  //         query,
-  //       });
-  //       return suggestions;
-  //     }
-  //   },
-  //   [searchPlugin]
-  // );
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (!open) {
+      setOpen(true);
+    }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  // const getSuggestionDebounce = React.useMemo(
-  //   () =>
-  //     debounce(
-  //       (
-  //         request: string,
-  //         callback: (suggestions: string[] | undefined) => void
-  //       ) => {
-  //         onGetSuggestions(request).then((data) => {
-  //           callback(data);
-  //         });
-  //       },
-  //       500
-  //     ),
-  //   [onGetSuggestions]
-  // );
+    if (event.key === "Enter") {
+      searchQuery(search);
+    }
+  };
 
-  // React.useEffect(() => {
-  //   let active = true;
-  //   if (search === "") {
-  //     setOptions([]);
-  //     return;
-  //   }
-  //   if (currentSearch === search) {
-  //     return;
-  //   }
+  const handleFocus = () => {
+    setOpen(true);
+  };
 
-  //   getSuggestionDebounce(search, (suggestions) => {
-  //     if (active) {
-  //       if (suggestions) {
-  //         setOptions(suggestions);
-  //       }
-  //     }
-  //   });
-
-  //   return () => {
-  //     active = false;
-  //   };
-  // }, [search, currentSearch, getSuggestionDebounce]);
-
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.currentTarget.value);
+  const handleSelect = (option: string) => {
+    setSelected(option);
+    searchQuery(option);
+    setSearch(option);
+    setOpen(false);
   };
 
   return (
-    <form className="flex-1" onSubmit={handleSubmit}>
-      <div className="flex bg-secondary p-[0.1rem] rounded-full items-center">
-        <Input
-          className="rounded-full rounded-r-none border-none"
-          placeholder={t("search")}
-          value={search}
-          onChange={onInputChange}
-        />
-        <button type="submit" className="px-5">
-          <CiSearch className="h-8 w-8 text-primary" />
-        </button>
+    <CommandPrimitive
+      onKeyDown={handleKeyDown}
+      className="rounded-full border flex-1"
+      shouldFilter={false}
+    >
+      <CommandInput
+        placeholder="Search"
+        onValueChange={onValueChange}
+        value={search}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+      />
+      <div className="relative">
+        {open && (
+          <div className="absolute top-1 z-50 w-full rounded-xl bg-stone-50 outline-none animate-in fade-in-0 zoom-in-95">
+            <CommandList className="ring-1 ring-slate-200 rounded-lg">
+              <CommandGroup>
+                {options.map((result) => (
+                  <CommandItem
+                    key={result}
+                    value={result}
+                    onSelect={handleSelect}
+                    onMouseDown={(event) => {
+                      event.stopPropagation();
+                      event.preventDefault();
+                    }}
+                  >
+                    {result}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </div>
+        )}
       </div>
-      {/* <SearchBarComponent
-        freeSolo
-        onInputChange={(_event, newInputValue) => {
-          setSearch(newInputValue);
-        }}
-        onChange={(_event: any, newValue: unknown) => {
-          if (typeof newValue === "string") {
-            searchQuery(newValue);
-            setCurrentSearch(newValue);
-          }
-        }}
-        renderInput={(params) => {
-          const { InputProps, ...rest } = params;
-          return (
-            <TextField
-              placeholder={t("search")}
-              {...rest}
-              InputProps={{
-                ...InputProps,
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Search />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          );
-        }}
-        options={options}
-        filterOptions={(x) => x}
-      /> */}
-    </form>
+    </CommandPrimitive>
   );
 };
+
 export default SearchBar;
