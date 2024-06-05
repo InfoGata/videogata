4675b7446e8799bb68cfa1c0ae56da1eadb0e8d2
Elijah Green <elijah@infogata.com>
2024-06-04 21:33:34 -0700

    Rename twitter to X

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -9,15 +9,15 @@ import {
   FaHeart,
   FaLock,
   FaMastodon,
-  FaTwitter,
+  FaXTwitter,
 } from "react-icons/fa6";
 
 const AboutPage: React.FC = () => {
   const { t } = useTranslation();
   const email = "contact@videogata.com";
   const website = "https://www.infogata.com";
-  const twitterUrl = "https://twitter.com/info_gata";
-  const twitterAt = "@info_gata";
+  const xUrl = "https://x.com/info_gata";
+  const xAt = "@info_gata";
   const mastodonUrl = "https://mastodon.online/@InfoGata";
   const mastodonAt = "@InfoGata@mastodon.online";
   const gitUrl = "https://github.com/InfoGata/videogata";
@@ -41,10 +41,10 @@ const AboutPage: React.FC = () => {
       icon: <FaEnvelope />,
     },
     {
-      title: "Twitter",
-      description: twitterAt,
-      icon: <FaTwitter />,
-      url: twitterUrl,
+      title: "X",
+      description: xAt,
+      icon: <FaXTwitter />,
+      url: xUrl,
     },
     {
       title: "Mastodon",
