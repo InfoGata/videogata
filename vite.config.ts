445e485255cd8acead0559804b66a75d4da60c81
Elijah Green <elijah@infogata.com>
2025-03-12 20:43:48 -0700

    Update @tanstack/router dependencies and change module resolution in tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    TanStackRouterVite(),
+    TanStackRouterVite({ target: "react" }),
     VitePWA({
       strategies: "injectManifest",
       srcDir: "src",
