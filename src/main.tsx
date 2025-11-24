5e532c52500b6b9e469d43a1901aecb6958fb81d
Elijah Green <elijah@infogata.com>
2025-11-23 19:34:51 -0800

    Fix ThemeProvider import in main.tsx
    Update import path to use the new contexts/ThemeContext location.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import "./i18n";
 import "./index.css";
-import { ThemeProvider } from "./providers/ThemeProvider";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import Router from "./router";
 import store, { persistor } from "./store/store";
 import { QueryClient, QueryClientProvider } from "react-query";
